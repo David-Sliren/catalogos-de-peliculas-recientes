707d12788ff1806e0a4f47b3af87141dd8d4d368
David Sliren <bluee.97.berry@gmail.com>
2026-01-10 23:46:41 -0500

    feact(fetchControl): implementar control de errores y loading cuando se hace la llamada fetch a los catalogos

---
+++ src/components/ErrorControl.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ErrorControl({ text }) {
+  return (
+    <span className="col-span-full text-8xl text-center text-black font-bebas_nue font-bold -tracking-tight">
+      {text}
+    </span>
+  );
+}
+
+export default ErrorControl;
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -4,6 +4,7 @@ import { useMovie } from "../hooks/useTankStack";
 import { useState } from "react";
 import { useParams } from "react-router";
 import Btn from "../components/Btn";
+import ErrorControl from "../components/ErrorControl";
 
 function Catalogo() {
   const [newpage, setNewPage] = useState(1);
@@ -22,14 +23,15 @@ function Catalogo() {
 
   console.log(catalogo.data);
 
-  if (catalogo.isLoading) {
-    return <div>Cargando</div>;
-  }
   return (
-    <main className=" bg-linear-to-r from-sky-500 min-h-dvh py-2 ">
+    <main className="flex flex-col justify-center items-center bg-linear-to-r from-sky-500 min-h-dvh py-2 ">
       {/* <header>Hola</header> */}
+
       <section className="grid grid-cols-2 w-full h-full justify-center gap-4 p-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6">
-        {catalogo.data.results?.map((item) => {
+        {catalogo.isError && <ErrorControl text={catalogo.error.message} />}
+
+        {catalogo.isLoading && <ErrorControl text="Cargando..." />}
+        {catalogo.data?.results?.map((item) => {
           return (
             <Card
               key={item.id}
@@ -39,6 +41,7 @@ function Catalogo() {
           );
         })}
       </section>
+
       <section className="flex justify-evenly items-center w-full py-2">
         <Btn text="Prev" onActive={prevPage} isLeft={true} />
         <span className="bg-black flex justify-center items-center size-10 text-lg font-inter font-semibold text-white rounded-full transition-discrete duration-500 hover:bg-white hover:text-zinc-900 ">
