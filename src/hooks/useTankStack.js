83581af7cbf57cc975c9e9e68d14b9d1bf6e824b
David Sliren <bluee.97.berry@gmail.com>
2026-01-10 19:44:46 -0500

    feact(fetchQuery): implementar fetchin dinamico, basado en las rutas dinamicas

--- src/hooks/useTankStack.js
+++ src/hooks/useTankStack.js
@@ -2,30 +2,19 @@ import { useQuery } from "@tanstack/react-query";
 
 import { callApi } from "./useFetch";
 
-export function useMovieRecent(page = 1) {
-  const recent = useQuery({
-    queryKey: ["recent", { page }],
+export function useMovie(route, page = 1) {
+  const url = route === "trending" ? `${route}/movie/day` : `movie/${route}`;
+
+  const catalogo = useQuery({
+    queryKey: ["catalogo", { route, page }],
     queryFn: () =>
       callApi(
-        `https://api.themoviedb.org/3/trending/movie/day?language=es-MX&page=${page}`
+        `https://api.themoviedb.org/3/${url}?language=es-MX&page=${page}`
       ),
     staleTime: 1 * 60 * 1000,
   });
 
   return {
-    recent,
-  };
-}
-
-export function useMoviePopular(page = 1) {
-  const url = `https://api.themoviedb.org/3/movie/popular?language=es-MX&page=${page}'`;
-
-  const popular = useQuery({
-    queryKey: ["moreView", { page }],
-    queryFn: () => callApi(url),
-  });
-
-  return {
-    popular,
+    catalogo,
   };
 }
--- src/pages/Catalogo.jsx
+++ src/pages/Catalogo.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import Card from "../components/Card";
-import { useMovieRecent, useMoviePopular } from "../hooks/useTankStack";
+import { useMovie } from "../hooks/useTankStack";
 import { useState } from "react";
+import { useParams } from "react-router";
 
 function Catalogo() {
   const [newpage, setNewPage] = useState(1);
-  const { popular } = useMoviePopular(newpage);
+  const { title } = useParams();
+  const { catalogo } = useMovie(title, newpage);
   function nextPage() {
     setNewPage((prev) => prev + 1);
-    console.log("page" + newpage);
   }
 
-  console.log(popular.data);
+  console.log(catalogo.data);
 
-  if (popular.isLoading) {
+  if (catalogo.isLoading) {
     return <div>Cargando</div>;
   }
   return (
     <main className="min-h-dvh">
       {/* <header>Hola</header> */}
       <section className="bg-zinc-800 grid grid-cols-2 w-full h-full justify-center gap-4 p-6 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 2xl:grid-cols-6">
-        {popular.data.results?.map((item) => {
+        {catalogo.data.results?.map((item) => {
           return (
             <Card
               key={item.id}
